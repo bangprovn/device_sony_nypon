b82fcd1f9b19e6690b13aa64dbb7935e01acb71c
Arnav Gupta <championswimmer@gmail.com>
2012-07-18 00:36:19 +0530

    just a precaution
    Signed-off-by: Arnav Gupta <championswimmer@gmail.com>

--- cm.mk
+++ cm.mk
@@ -1,7 +1,7 @@
 # Release name
 PRODUCT_RELEASE_NAME := LT22i
 # Inherit some common CM stuff.
-$(call inherit-product, vendor/cm/config/common_full_phone.mk)
+$(call inherit-product-if-exists, vendor/cm/config/common_full_phone.mk)
 # Inherit device configuration
 $(call inherit-product, device/sony/nypon/full_nypon.mk)
 ## Device identifier. This must come after all inclusions
