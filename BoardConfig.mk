9a8834496821fac436462c5e0c7ff0dbb41f19cf
Keiran Collins <keiran@darkforestgroup.com>
2012-07-18 23:09:48 +0200

    Use axp_nypon_defconfig.
    Also, nypon is Xperia P :)

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -1,9 +1,9 @@
 -include device/sony/u8500_common/BoardConfigCommon.mk
 
 TARGET_KERNEL_SOURCE := kernel/sony/u8500
-#TARGET_KERNEL_CONFIG := cyanogen_zeus_defconfig
+TARGET_KERNEL_CONFIG := axp_nypon_defconfig
 TARGET_PREBUILT_KERNEL := device/sony/nypon/prebuilt/kernel
 
 BOARD_CUSTOM_RECOVERY_KEYMAPPING := ../../device/sony/nypon/recovery/recovery_keys.c
 
-TARGET_OTA_ASSERT_DEVICE := LT22i,LT22a,nypon,Sola,LT22
+TARGET_OTA_ASSERT_DEVICE := LT22i,LT22a,nypon,P,LT22
